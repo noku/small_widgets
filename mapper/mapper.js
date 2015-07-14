2d7e984b1ed655bcd9772bf1506dfae25a4cbf84
peter <negrei.petru@gmail.com>
2015-07-14 21:13:14 +0300

    add functionality for the marker

--- mapper/mapper.js
+++ mapper/mapper.js
@@ -6,11 +6,33 @@
     }
 
     Mapper.prototype = {
-      _on : function(event, callback){
+
+      _on : function(opts){
         var self = this;
-        google.maps.event.addListener(this.gMap, event, function(e){
-          callback.call(self, e)
+        google.maps.event.addListener(opts.obj, opts.event, function(e){
+          opts.callback.call(self, e)
         });
+      },
+
+      addMarker: function(opts){
+        var marker;
+        opts.position = {
+          lat : opts.lat,
+          lng : opts.lng
+        }
+        marker = this._createMarker(opts);
+        if(opts.event){
+          this._on({
+            obj: marker,
+            event: opts.event.name,
+            callback: opts.event.callback
+          })
+        }
+      },
+
+      _createMarker: function(opts){
+        opts.map = this.gMap;
+        return new google.maps.Marker(opts);
       }
     }
 
--- mapper/script.js
+++ mapper/script.js
@@ -4,10 +4,17 @@
       element = document.getElementById('map-canvas'),
       map = mapper.create(element, options);
 
-  map._on("click", function(e){
-    console.log('click');
-    console.log(e);
-    console.log(this);
-  })
+  map.addMarker({
+    lat: 37.791350,
+    lng: -122.435883,
+    draggable: true,
+    event: {
+      name : "click",
+      callback: function(){
+        alert("I am clicked");
+      }
+    },
+    icon: "http://mapicons.nicolasmollet.com/wp-content/uploads/mapicons/shape-default/color-9d7050/shapecolor-color/shadow-1/border-dark/symbolstyle-white/symbolshadowstyle-dark/gradient-no/pickup_camper.png"
+  });
   
 }(window, google, window.Mapper))
