07ec9b752517e9144d061c25705659c37ee44416
peter <negrei.petru@gmail.com>
2015-07-17 21:13:14 +0300

    improving the search marke code

--- mapper/js/search.js
+++ mapper/js/search.js
@@ -1,27 +1,35 @@
 $(function() {
 
   var availableTags = [],
-      db = DB.create("maps");
+      db = DB.create("maps"),
+      gMap = mapper.mapper("getMap");
+
 
   db.MARKERS.forEach(function(marker){
     availableTags.push(marker.name);        
   });
   
-  var setPosition = function(lat, lng){
-      var gMap = mapper.mapper("getMap")
+  var setPosition = function(lat, lng, found){
       gMap.setCenter(new google.maps.LatLng( lat, lng ) );
       gMap.setZoom(8);  
+      found.setAnimation(google.maps.Animation.BOUNCE);
+      setTimeout(function(){ found.setAnimation(null); }, 3750);
   }
 
-
   $('#marker').bind('keypress', function(e) {
+    // on enter
     if(e.keyCode==13){
       var query = $("#marker").val(),
+          // array of markers
           found = mapper.mapper("findMarkers", function(marker){
             return marker.name === query
           });
 
-      setPosition(found[0].lat,found[0].lng);
+      if(found.length > 0){
+        setPosition(found[0].lat,found[0].lng, found[0]);      
+      } else {
+        alert("Marker with this name not found");
+      }
     }
   });
 
